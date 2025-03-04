7e119a1840ace928b4fe3238f3c41f922b6eae0d
Oybek Abdujabborov <abdujabborovoybek@gmail.com>
2025-03-04 08:14:04 +0500

    [deploy] client

--- src/config/mysql2.config.js
+++ src/config/mysql2.config.js
@@ -1,7 +1,9 @@
 const mysql = require("mysql2/promise");
 
+const production = process.env.NODE_ENV === "production";
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: production ? "localhost" : process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
